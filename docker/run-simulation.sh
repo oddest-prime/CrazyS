f7e29378bfcef3844091dd61098c8a9274616b5d
Andreas B <andreas.brandstaetter@tuwien.ac.at>
2020-10-14 11:42:12 +0200

    add xvfb

--- docker/run-simulation.sh
+++ docker/run-simulation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+Xvfb -shmem -screen 0 1280x1024x24 &
+export DISPLAY=:0
+
 cd /crazyflie_ws
 source /opt/ros/melodic/setup.bash
 source /crazyflie_ws/devel/setup.bash
@@ -7,5 +10,7 @@ source /crazyflie_ws/devel/setup.bash
 catkin build
 
 roslaunch rotors_gazebo crazyflie2_hovering_swarm.launch gui:=false
+
+kill `pidof Xvfb`
 
 /crazyflie_ws/src/crazys/docker/generate-video.sh

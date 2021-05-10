09a1a868876b82977b13dad8d1d3bd84de4821bd
andreas <andreas@thrymr.vmars.tuwien.ac.at>
2021-05-10 17:47:43 +0200

    run every execution in fresh docker container on thrymr

--- docker/run-simulation.sh
+++ docker/run-simulation.sh
@@ -6,14 +6,18 @@ Xvfb -shmem -screen 0 1280x1024x24 &
 export DISPLAY=:0
 
 hash="uu"
-if [ $# -ge 1 ]
+mode="mm"
+if [ $# -ge 2 ]
 then
   hash="$1"
+  mode="$2"
 fi;
 date_hash="`date +%Y-%m-%d_%H-%M-%S`_${hash}"
+date_hash_mode="${date_hash}_${mode}"
 echo "========================================================="
 echo "git hash: ${hash}"
-echo "date and hash: ${date_hash}"
+echo "swarm mode: ${mode}"
+echo "date, hash and mode: ${date_hash_mode}"
 echo "========================================================="
 
 cd /crazyflie_ws
@@ -31,13 +35,8 @@ echo "========================================================="
 # roslaunch rotors_gazebo crazyflie2_swarm15.launch gui:=false & # big swarm with 15 quadcopters
 # roslaunch rotors_gazebo crazyflie2_swarm.launch gui:=false & # small swarm with 4 quadcopters
 
-date_hash_info="${date_hash}_mpc1"
-echo "========================================================="
-echo "running ${date_hash_info}"
-echo "========================================================="
-
 mkdir -p /crazyflie_ws/src/crazys/log_output
-roslaunch rotors_gazebo crazyflie2_swarm15.launch gui:=false swarm_mode:=mpc1 & # big swarm with 15 quadcopters
+roslaunch rotors_gazebo crazyflie2_swarm15.launch gui:=false swarm_mode:=${mode} & # big swarm with 15 quadcopters
 
 sleep 1
 iter=1
@@ -56,35 +55,6 @@ kill $R_PID 2> /dev/null
 sleep 5
 kill `pidof Xvfb`
 
-/crazyflie_ws/src/crazys/docker/generate-video.sh "${date_hash_info}"
-mv /crazyflie_ws/src/crazys/log_output /crazyflie_ws/src/crazys/log_${date_hash_info}
+/crazyflie_ws/src/crazys/docker/generate-video.sh "${date_hash_mode}"
+mv /crazyflie_ws/src/crazys/log_output /crazyflie_ws/src/crazys/log_${date_hash_mode}
 
-
-
-date_hash_info="${date_hash}_reynolds"
-echo "========================================================="
-echo "running ${date_hash_info}"
-echo "========================================================="
-
-mkdir -p /crazyflie_ws/src/crazys/log_output
-roslaunch rotors_gazebo crazyflie2_swarm15.launch gui:=false swarm_mode:=reynolds & # big swarm with 15 quadcopters
-
-sleep 1
-iter=1
-R_PID=`pgrep roslaunch`
-for i in `seq 1 $TIMEOUT`
-do
-    kill -0 $R_PID 2> /dev/null || break
-    echo "  [[ global sim timeout counter: $iter / $TIMEOUT ]]  "
-    iter=$((iter+1))
-    sleep 1
-done;
-echo $iter > /tmp/run.seconds
-echo ""
-echo "##  killing roslaunch";
-kill $R_PID 2> /dev/null
-sleep 5
-kill `pidof Xvfb`
-
-/crazyflie_ws/src/crazys/docker/generate-video.sh "${date_hash_info}"
-mv /crazyflie_ws/src/crazys/log_output /crazyflie_ws/src/crazys/log_${date_hash_info}
---
+++ docker/run-thrymr.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ~/SWARM/crazys
+docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` mpc1
+docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` reynolds
+
+

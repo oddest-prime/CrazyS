660b33e28fcbc0bdc53a0edb58ea03e819449f54
Andreas B <andreas.brandstaetter@tuwien.ac.at>
2022-02-28 21:09:06 +0100

    adjust full run to check gradient with linear pdf gain

--- docker/run-thrymr-full.sh
+++ docker/run-thrymr-full.sh
@@ -7,6 +7,54 @@ cd ~/SWARM/crazys
 
 # full simulation run, February 2022
 #for params in "spc_global_A" "spc_local_A" "spc_global_B" "spc_local_B"
+for params in "x"
+do
+  for obstacles in "" "_obstacle2" "_obstacle3"
+  do
+    echo "running: params=$params obstacles=$obstacles"
+
+    docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` swarm30$obstacles gradient spc_local_A "full" &
+    docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` swarm30$obstacles gradient spc_local_B "full" &
+    sleep 150 # delay compilation by 150 seconds in second two docker containers
+    docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` swarm15$obstacles gradient spc_local_A "full" &
+    docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` swarm15$obstacles gradient spc_local_B "full" &
+    wait
+
+    test -e /tmp/stop && exit 7 # stop if flag is present
+
+    docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` swarm9$obstacles gradient spc_local_A "full" &
+    docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` swarm9$obstacles gradient spc_local_B "full" &
+    sleep 150 # delay compilation by 150 seconds in second two docker containers
+    docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` swarm4$obstacles gradient spc_local_A "full" &
+    docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` swarm4$obstacles gradient spc_local_B "full" &
+    wait
+
+    test -e /tmp/stop && exit 7 # stop if flag is present
+  done
+done
+
+RUN_END=`date`
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo "% RUN_START: ${RUN_START}"
+echo "% RUN_END:   ${RUN_END}"
+echo "%   run-thrymr-full.sh done."
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+
+
+
+
+
+
+
+
+
+
+
+
+exit 0; # ###################################
+
+# full simulation run, February 2022
+#for params in "spc_global_A" "spc_local_A" "spc_global_B" "spc_local_B"
 for params in "spc_local_A" "spc_local_B"
 do
   for obstacles in "" "_obstacle2" "_obstacle3"

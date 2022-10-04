0de1ec931dc8f8886544437168af0d9532c6858a
Andreas B <andreas.brandstaetter@tuwien.ac.at>
2022-10-03 22:21:56 -0400

    test different sep weights

--- docker/run-thrymr-full.sh
+++ docker/run-thrymr-full.sh
@@ -2,8 +2,46 @@
 
 RUN_START=`date`
 rm -f /tmp/stop
-
 cd ~/SWARM/crazys
+
+# test different separation weights
+for i in a b c
+do
+  for j in 50 70 100 150 200 250 300 350 400 500 600 700 800 900 1000 2000 # dyn_sep
+  do
+    dyn_sep="j"
+    echo "i = $i, j = $j, dyn_sep = $dyn_sep"
+
+    cp rotors_gazebo/resource/crazyflie2_mpc1_placeholder.yaml rotors_gazebo/resource/crazyflie2_mpc1_dyn_a.yaml
+    sed -i "s/__DYN_SEP__/$dyn_sep/g" rotors_gazebo/resource/crazyflie2_mpc1_dyn_a.yaml
+
+    docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` dist15 dist mpc1_dyn_a 0 4 "dyn-sep_${dyn_sep}" &
+    docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` dist9 dist mpc1_params2a 0 4 "dyn-sep_${dyn_sep}" &
+    sleep 150 # delay compilation by 150 seconds in second two docker containers
+    docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` dist5 dist mpc1_dyn_a 0 4 "dyn-sep_${dyn_sep}" &
+    docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` dist2 dist mpc1_params2a 0 4 "dyn-sep_${dyn_sep}" &
+    wait
+
+    rm -rf rotors_gazebo/resource/crazyflie2_mpc1_dyn_a.yaml
+
+    test -e /tmp/stop && exit 7 # stop if flag is present
+  done
+done
+
+RUN_END=`date`
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo "% RUN_START: ${RUN_START}"
+echo "% RUN_END:   ${RUN_END}"
+echo "%   run-thrymr-full.sh done."
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+exit 0
+
+
+
+
+
+
+
 
 # full simulation run, Sept 2022
 #for params in "spc_global_A" "spc_local_A" "spc_global_B" "spc_local_B"

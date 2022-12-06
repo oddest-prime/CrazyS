2e98e51d00a9d25611d28edb92ff832d62c0c6f4
Andreas B <andreas.brandstaetter@tuwien.ac.at>
2022-12-06 11:13:05 -0500

    test for extra distance

--- docker/run-thrymr-full.sh
+++ docker/run-thrymr-full.sh
@@ -70,7 +70,7 @@ do
 #  for j in 1 2 5 7 10 12 15 17 20 25 30 50 120 150 # dyn_thr
 #  for j in 70 100 150 250 350 500 700 1000 1500 2500 3500 # dyn_tar
 #  for j in 5 10 20 50 100 200 250 500 1000 # dyn_sca
-#  for j in 0 5 10 20 35 50 75 100 150 200 300 500 # dyn_cal
+#  for j in 0 2 5 7 10 15 22 30 50 75 110 200 # dyn_cal
 #  for j in 5 7 10 15 20 35 50 70 100 # dyn_eps
 #  for j in 0 1 2 3 5 7 10 15 20 35 50 # dyn_nse
 #  for j in 2 5 7 8 10 12 15 20 35 50 70 100 150 200 # dyn_eps
@@ -82,7 +82,7 @@ do
 #  for j in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
 #  for j in 10 15 17 20 23 25 27 30 33 35 40 45 50 990
 #  for j in 5 6 7 8 9 10 12 15 20 # dyn_eps
-for j in 0 2 5 7 10 15 22 30 50 75 110 200 # dyn_cal
+for j in 10 15 20 30 50 75 120 200 # dyn_ese
   do
     #yamlname=`pwgen -n 4 1`
     yamlname=`printf "%05d%s" $j $i`
@@ -97,11 +97,34 @@ for j in 0 2 5 7 10 15 22 30 50 75 110 200 # dyn_cal
     dyn_tar="250" # "200" # "250" # "150"
     #dyn_sca=`echo "scale=2;$j / 100" | bc | awk '{printf "%.2f", $0}'`
     dyn_sca="2"
-    dyn_cal="$j" # 30, 5
-    #dyn_ese=`echo "scale=2;$j / 100" | bc | awk '{printf "%.2f", $0}'`
-    dyn_ese="0.1"
+    dyn_cal="0" # 30, 5
+    dyn_ese=`echo "scale=2;$j / 100" | bc | awk '{printf "%.2f", $0}'`
+    #dyn_ese="0.1"
     #dyn_nhd=`echo "scale=1;$j / 10" | bc | awk '{printf "%.1f", $0}'`
     dyn_nhd="2.5"
+
+    if [ "j" -eq "15" ]; then
+      dyn_sep="950"
+    fi;
+    if [ "j" -eq "20" ]; then
+      dyn_sep="900"
+    fi;
+    if [ "j" -eq "30" ]; then
+      dyn_sep="800"
+    fi;
+    if [ "j" -eq "50" ]; then
+      dyn_sep="610"
+    fi;
+    if [ "j" -eq "75" ]; then
+      dyn_sep="420"
+    fi;
+    if [ "j" -eq "120" ]; then
+      dyn_sep="200"
+    fi;
+    if [ "j" -eq "200" ]; then
+      dyn_sep="25"
+    fi;
+
     echo "i = $i, j = $j, dyn_nse = $dyn_nse, dyn_eps = $dyn_eps, dyn_nmm = $dyn_nmm, dyn_sep = $dyn_sep, dyn_thr = $dyn_thr, dyn_tar = $dyn_tar, dyn_sca = $dyn_sca, dyn_cal = $dyn_cal, dyn_ese = $dyn_ese, dyn_nhd = $dyn_nhd"
 
     cp rotors_gazebo/resource/crazyflie2_mpc1_placeholder.yaml rotors_gazebo/resource/crazyflie2_mpc1_dyn_${yamlname}.yaml
@@ -120,7 +143,7 @@ for j in 0 2 5 7 10 15 22 30 50 75 110 200 # dyn_cal
     #docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` dist2_rover dist mpc1_dyn_${yamlname} 0 6 "${extratext}" &
     #docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` dist9_rover dist mpc1_dyn_${yamlname} 0 6 "${extratext}" &
 
-    extratext="dyn_cal${dyn_cal}"
+    extratext="dyn_ese${dyn_ese}dyn_sep${dyn_sep}"
     docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` dist15 dist mpc1_dyn_${yamlname} 0 5 "${extratext}" &
     wait_until_max_procs_running
     #docker run --rm --volume ~/SWARM/crazys:/crazyflie_ws/src/crazys crazys /crazyflie_ws/src/crazys/docker/run-simulation.sh `git rev-parse --short HEAD` dist15 distGT mpc1_dyn_${yamlname} 0 5 "${extratext}" &
